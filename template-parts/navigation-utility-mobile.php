23a8b76b4549cafedd5a9a622993a88ca37014bf
Sunny <sunnywz@gmail.com>
2024-10-29 14:24:06 -0500

    utility navigation active site

---
+++ template-parts/navigation-utility-mobile.php
@@ -0,0 +1,24 @@
+<div class="mobile-utility-container">
+    <ul class="utility-navigation">
+        <li class="current-site">
+            <a href="https://newmanministry.com/">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/ministry-logo-white-min.png" alt="Newman Ministry" class="full-white-logo" />
+            </a>
+        </li>
+        <li>
+            <a href="https://connection.newmanministry.com/" class="">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/connection-logo-white-min.png" alt="Newman Connection" class="full-white-logo" />
+            </a>
+        </li>
+        <li>
+            <a href="https://housing.newmanministry.com/" class="">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/housing-logo-white-min.png" alt="Newman Housing" class="full-white-logo" />
+            </a>
+        </li>
+        <li>
+            <a href="https://called.newmanministry.com/" class="">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/called-logo-white-min.png" alt="Called" class="full-white-logo" />
+            </a>
+        </li>
+    </ul>
+</div>
---
+++ template-parts/navigation-utility.php
@@ -0,0 +1,34 @@
+<aside class="utility-navigation-container">
+	<div class="inner">
+		<ul class="utility-navigation">
+			<li>
+				<a href="https://newmanministry.com/" class="">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/ministry-icon-white-min.png" alt="Newman Ministry" class="white-logo" />
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/ministry-icon-color-min.png" alt="Newman Ministry" class="color-logo" />
+					<?php esc_html_e("Newman Ministry", "newman-base"); ?>
+				</a>
+			</li>
+			<li>
+				<a href="https://connection.newmanministry.com/" class="">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/connection-icon-white-min.png" alt="Newman Connection" class="white-logo" />
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/connection-icon-color-min.png" alt="Newman Connection" class="color-logo" />
+					<?php esc_html_e("Newman Connection", "newman-base"); ?>
+				</a>
+			</li>
+			<li>
+				<a href="https://housing.newmanministry.com/" class="active">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/housing-icon-white-min.png" alt="Newman Student Housing" class="white-logo" />
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/housing-icon-color-min.png" alt="Newman Student Housing" class="color-logo" />
+					<?php esc_html_e("Newman Student Housing", "newman-base"); ?>
+				</a>
+			</li>
+			<li>
+				<a href="https://called.newmanministry.com/" class="">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/called-icon-white-min.png" alt="Called" class="white-logo" />
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/called-icon-color-min.png" alt="Called" class="color-logo" />
+					<?php esc_html_e("Called", "newman-base"); ?>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
